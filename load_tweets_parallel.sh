8b09f5b2f137568c9fd97e9f1bdaa3e727cde935
smfl2022 <sophyfigaroa@gmail.com>
2026-04-24 16:42:29 -0700

    Step 0 and half of step 1

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -16,6 +16,7 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: copy your solution to the previous problem here
+time echo "$files" | parallel ./load_denormalized.sh
 
 # NOTE:
 # I have removed the pg_normalized code from this repo.
@@ -27,3 +28,4 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: copy your solution to the previous problem here
+time echo "$files" | parallel ./load_normalized_batch.sh
