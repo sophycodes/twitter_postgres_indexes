20fea0d6c447dd9fdbcc3086b369ddc82b613509
smfl2022 <sophyfigaroa@gmail.com>
2026-04-26 13:19:11 -0700

    fix url->urls column names, use absolute path in load script

--- load_normalized_batch.sh
+++ load_normalized_batch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:7824/postgres --inputs $1
+python3 -u /home/smfl2022/hw/twitter_postgres_indexes/load_tweets_batch.py --db=postgresql://postgres:pass@localhost:7824/postgres --inputs $1
--- load_tweets_batch.py
+++ load_tweets_batch.py
@@ -189,7 +189,7 @@ def _insert_tweets(connection,input_tweets):
             'screen_name':remove_nulls(tweet['user']['screen_name']),
             'name':remove_nulls(tweet['user']['name']),
             'location':remove_nulls(tweet['user']['location']),
-            'url':tweet['user']['url'], 
+            'urls':tweet['user']['url'], 
             'description':remove_nulls(tweet['user']['description']),
             'protected':tweet['user']['protected'],
             'verified':tweet['user']['verified'],
@@ -295,7 +295,7 @@ def _insert_tweets(connection,input_tweets):
         for url in urls:
             tweet_urls.append({
                 'id_tweets':tweet['id'],
-                'url':url['expanded_url'],
+                'urls':url['expanded_url'],
                 })
 
         ########################################
@@ -353,7 +353,7 @@ def _insert_tweets(connection,input_tweets):
         for medium in media:
             tweet_media.append({
                 'id_tweets':tweet['id'],
-                'url':medium['media_url'],
+                'urls':medium['media_url'],
                 'type':medium['type']
                 })
 
